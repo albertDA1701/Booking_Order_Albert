91ce0ca1c84ff339b316d8d38b815613a7b5dac0
albertDA1701 <101168428+albertDA1701@users.noreply.github.com>
2022-03-18 15:52:16 +0700

    Add files via upload

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -2,20 +2,20 @@
 # from odoo import http
 
 
-# class BookingOrderAlbertusDa(http.Controller):
-#     @http.route('/booking_order__albertus_da/booking_order__albertus_da/', auth='public')
+# class BookingOrderAlbert(http.Controller):
+#     @http.route('/booking_order_albert/booking_order_albert/', auth='public')
 #     def index(self, **kw):
 #         return "Hello, world"
 
-#     @http.route('/booking_order__albertus_da/booking_order__albertus_da/objects/', auth='public')
+#     @http.route('/booking_order_albert/booking_order_albert/objects/', auth='public')
 #     def list(self, **kw):
-#         return http.request.render('booking_order__albertus_da.listing', {
-#             'root': '/booking_order__albertus_da/booking_order__albertus_da',
-#             'objects': http.request.env['booking_order__albertus_da.booking_order__albertus_da'].search([]),
+#         return http.request.render('booking_order_albert.listing', {
+#             'root': '/booking_order_albert/booking_order_albert',
+#             'objects': http.request.env['booking_order_albert.booking_order_albert'].search([]),
 #         })
 
-#     @http.route('/booking_order__albertus_da/booking_order__albertus_da/objects/<model("booking_order__albertus_da.booking_order__albertus_da"):obj>/', auth='public')
+#     @http.route('/booking_order_albert/booking_order_albert/objects/<model("booking_order_albert.booking_order_albert"):obj>/', auth='public')
 #     def object(self, obj, **kw):
-#         return http.request.render('booking_order__albertus_da.object', {
+#         return http.request.render('booking_order_albert.object', {
 #             'object': obj
 #         })
